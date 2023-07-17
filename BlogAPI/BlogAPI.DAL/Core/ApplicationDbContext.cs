2ff98a0cbcb050c965ce6e01da4391b7d76554e4
kek145 <yurabogomolov2004@gmail.com>
2023-07-17 15:15:49 +0300

    UPDATE | Added entities and connections

--- BlogAPI/BlogAPI.DAL/Core/ApplicationDbContext.cs
+++ BlogAPI/BlogAPI.DAL/Core/ApplicationDbContext.cs
@@ -1,35 +1,71 @@
-﻿using BlogAPI.Domain.Entity.Connection;
+﻿using System;
 using BlogAPI.Domain.Entity.Table;
 using Microsoft.EntityFrameworkCore;
+using BlogAPI.Domain.Entity.Connection;
 
 namespace BlogAPI.DAL.Core;
 
 public class ApplicationDbContext : DbContext
 {
-    public DbSet<UserEntity> User { get; set; }
-    public DbSet<RoleEntity> Role { get; set; }
-    public DbSet<UserRoleEntity> UserRole { get; set; }
-    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
+    public DbSet<UserEntity> User { get; set; } = null!;
+    public DbSet<RoleEntity> Role { get; set; } = null!;
+    public DbSet<CommentEntity> Comment { get; set; } = null!;
+    public DbSet<ArticleEntity> Article { get; set; } = null!;
+    public DbSet<CategoryEntity> Category { get; set; } = null!;
 
+    public DbSet<UserRoleEntity> UserRole { get; set; } = null!;
+    public DbSet<UserArticleEntity> UserArticle { get; set; } = null!;
+    public DbSet<UserCommentEntity> UserComment { get; set; } = null!;
+    public DbSet<ArticleCommentEntity> ArticleComment { get; set; } = null!;
+    public DbSet<ArticleCategoryEntity> ArticleCategory { get; set; } = null!;
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<UserEntity>().ToTable("Users");
         modelBuilder.Entity<RoleEntity>().ToTable("Roles");
+        modelBuilder.Entity<CommentEntity>().ToTable("Comments");
+        modelBuilder.Entity<ArticleEntity>().ToTable("Articles");
+        modelBuilder.Entity<CategoryEntity>().ToTable("Categories");
+
         modelBuilder.Entity<UserRoleEntity>().ToTable("UsersRoles");
-        
+        modelBuilder.Entity<UserArticleEntity>().ToTable("UsersArticles");
+        modelBuilder.Entity<UserCommentEntity>().ToTable("UsersComments");
+        modelBuilder.Entity<ArticleCommentEntity>().ToTable("ArticlesComments");
+        modelBuilder.Entity<ArticleCategoryEntity>().ToTable("ArticleCategories");
+
         modelBuilder.Entity<UserEntity>().HasKey(user => user.UserId);
         modelBuilder.Entity<RoleEntity>().HasKey(role => role.RoleId);
+        modelBuilder.Entity<CommentEntity>().HasKey(comment => comment.CommentId);
+        modelBuilder.Entity<ArticleEntity>().HasKey(article => article.ArticleId);
+        modelBuilder.Entity<CategoryEntity>().HasKey(category => category.CategoryId);
 
         modelBuilder.Entity<UserEntity>().HasIndex(user => user.Email).IsUnique();
         modelBuilder.Entity<RoleEntity>().HasIndex(role => role.RoleName).IsUnique();
-        
+        modelBuilder.Entity<CategoryEntity>().HasIndex(category => category.CategoryName).IsUnique();
+
         modelBuilder.Entity<RoleEntity>(builder =>
         {
             builder.Property(role => role.RoleName).HasMaxLength(20).IsRequired();
         });
+        modelBuilder.Entity<CategoryEntity>(builder =>
+        {
+            builder.Property(comment => comment.CategoryName).HasMaxLength(50).IsRequired();
+        });
+
+        modelBuilder.Entity<CommentEntity>(builder =>
+        {
+            builder.Property(comment => comment.Comment).HasMaxLength(2000).IsRequired();
+            builder.Property(comment => comment.CreatedAt).HasDefaultValue(DateTime.UtcNow);
+        });
         
-        
-        
+        modelBuilder.Entity<ArticleEntity>(builder =>
+        {
+            builder.Property(article => article.Title).HasMaxLength(200).IsRequired();
+            builder.Property(article => article.Content).HasMaxLength(4000).IsRequired();
+            builder.Property(article => article.CreatedAt).HasDefaultValue(DateTime.UtcNow);
+        });
+
         modelBuilder.Entity<UserEntity>(builder =>
         {
             builder.Property(user => user.FirstName).HasMaxLength(50).IsRequired();
@@ -44,18 +80,70 @@ public class ApplicationDbContext : DbContext
             new RoleEntity { RoleId = 2, RoleName = "Author" },
             new RoleEntity { RoleId = 3, RoleName = "User" }
         );
-
-        modelBuilder.Entity<UserRoleEntity>()
-            .HasKey(ur => new { ur.UserId, ur.RoleId });
         
         modelBuilder.Entity<UserRoleEntity>()
-            .HasOne(ur => ur.User)
-            .WithMany(ur => ur.UserRole)
-            .HasForeignKey(ur => ur.UserId);
+                .HasKey(ur => new { ur.UserId, ur.RoleId });
+        
+        modelBuilder.Entity<UserRoleEntity>()
+                .HasOne(ur => ur.User)
+                .WithMany(ur => ur.UserRole)
+                .HasForeignKey(ur => ur.UserId);
             
         modelBuilder.Entity<UserRoleEntity>()
             .HasOne(ur => ur.Role)
             .WithMany(ur => ur.UserRole)
             .HasForeignKey(ur => ur.RoleId);
+            
+        modelBuilder.Entity<UserArticleEntity>()
+            .HasKey(ua => new { ua.ArticleId, ua.UserId });
+            
+        modelBuilder.Entity<UserArticleEntity>()
+            .HasOne(ua => ua.Article)
+            .WithMany(ua => ua.UserArticle)
+            .HasForeignKey(ua => ua.ArticleId);
+            
+        modelBuilder.Entity<UserArticleEntity>()
+            .HasOne(ua => ua.User)
+            .WithMany(ua => ua.UserArticle)
+            .HasForeignKey(ua => ua.UserId);
+            
+        modelBuilder.Entity<UserCommentEntity>()
+            .HasKey(uc => new { uc.UserId, uc.CommentId });
+            
+        modelBuilder.Entity<UserCommentEntity>()
+            .HasOne(uc => uc.User)
+            .WithMany(uc => uc.UserComment)
+            .HasForeignKey(uc => uc.UserId);
+            
+        modelBuilder.Entity<UserCommentEntity>()
+            .HasOne(uc => uc.Comment)
+            .WithMany(uc => uc.UserComment)
+            .HasForeignKey(uc => uc.CommentId);
+            
+        modelBuilder.Entity<ArticleCommentEntity>()
+            .HasKey(ac => new { ac.ArticleId, ac.CommentId });
+            
+        modelBuilder.Entity<ArticleCommentEntity>()
+            .HasOne(ac => ac.Comment)
+            .WithMany(ac => ac.ArticleComment)
+            .HasForeignKey(ac => ac.CommentId);
+            
+        modelBuilder.Entity<ArticleCommentEntity>()
+            .HasOne(ac => ac.Article)
+            .WithMany(ac => ac.ArticleComment)
+            .HasForeignKey(ac => ac.ArticleId);
+            
+        modelBuilder.Entity<ArticleCategoryEntity>()
+            .HasKey(ac => new { ac.ArticleId, ac.CategoryId });
+            
+        modelBuilder.Entity<ArticleCategoryEntity>()
+            .HasOne(ac => ac.Article)
+            .WithMany(ac => ac.ArticleCategory)
+            .HasForeignKey(ac => ac.ArticleId);
+            
+        modelBuilder.Entity<ArticleCategoryEntity>()
+            .HasOne(ac => ac.Category)
+            .WithMany(ac => ac.ArticleCategory)
+            .HasForeignKey(ac => ac.CategoryId);
     }
 }
