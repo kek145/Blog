5745deb7ca7830194b7f6d085cf19582d89a4c89
kek145 <yurabogomolov2004@gmail.com>
2023-07-17 09:47:22 +0300

    UPDATE | Added User, Role, UserRole entities

---
+++ BlogAPI/BlogAPI.Domain/Entity/Table/RoleEntity.cs
@@ -0,0 +1,10 @@
+﻿using BlogAPI.Domain.Entity.Connection;
+
+namespace BlogAPI.Domain.Entity.Table;
+
+public class RoleEntity
+{
+    public int RoleId { get; set; }
+    public string RoleName { get; set; }
+    public ICollection<UserRoleEntity> UserRole { get; set; }
+}
