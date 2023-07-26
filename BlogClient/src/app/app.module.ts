4a82f4dc71a8f0e231b2c54c621dd1f041a83342
kek145 <yurabogomolov2004@gmail.com>
2023-07-26 21:58:20 +0300

    UPDATE | Added basic routing

--- BlogClient/src/app/app.module.ts
+++ BlogClient/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegistrationComponent } from './components/registration/registration.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
